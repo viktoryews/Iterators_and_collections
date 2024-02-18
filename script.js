5e1c2a82d105a5ed17d214f7def7608fa42fb256
Viktorya Evsina <viktoryews1997@mail.ru>
2024-02-18 20:43:23 +0300

    *

---
+++ script.js
@@ -0,0 +1,55 @@
+/**
+ * 1
+ */
+
+//Создание объекта "Музыкальная коллекция"
+const musicCollection = {
+    //массив альбомов
+    albums: [
+        {
+            title: 'Царица',
+            artist: 'Анна Асти',
+            year: '2023',
+        },
+        {
+            title: 'I Like it',
+            artist: 'Enrique Iglesias',
+            year: '2010',
+        },
+        {
+            title: 'Brand New Day',
+            artist: 'Sting',
+            year: '1999',
+        },
+    ]
+};
+
+//Иттерирование объекта "Музыкальная коллекция"
+
+musicCollection[Symbol.iterator] = function() {
+    const array = this.albums; //получаем массив альбомов
+    let current = 0;
+    return {
+        next() {
+            if (current < array.length) {
+                return {
+                    value: array[current++],
+                    done: false
+                };
+            } else {
+                return {
+                    value: undefined,
+                    done: true
+                };
+            };
+        }
+    };
+};
+
+for (const album of musicCollection) {
+console.log(`${album.title} - ${album.artist} (${album.year})`);
+};
+
+/**
+ * 2
+ */
